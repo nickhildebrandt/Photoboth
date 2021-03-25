701118d35ef990124a5c4b2d7f01a699035a5648
Nick Hildebrandt <n.hildebrandt@ts13.de>
2021-03-25 19:12:24 +0100

    PIP

--- photoboth.py
+++ photoboth.py
@@ -1,43 +1,61 @@
-#!/usr/bin/env python3
-# ------------------------------------------------------------------
-# [Nick Hildebrandt] MacAndMore
-#          (C)2021
-# Photoboth for PI
-# ------------------------------------------------------------------
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import time
-import RPi.GPIO as GPIO
 import picamera
+import os
 import time
-import pi3d
+from PIL import Image, ImageDraw, ImageFont
 
-W, H = 800, 600
+def main():
+    """
+    Displayes preview on PiTFT with PiCamera.add_overlay(). Displayed animation
+    is composed of two layers; One continuously displays the input from camera
+    while another continuously gets refreshed every time a calculation is done.
+    It worls faster because the layers are divided and the original preview
+    layer does not get affected by another layer that requires some calculation
+    and image creation.
+    It, however, has a weak point. This outputs to default display device,
+    /dev/fb0. To display the same output to PiTFT, /dev/fb1, you need to use
+    rpi-fbcp and let it copy the /dev/fb0 output to /dev/fb1.
+    """
 
-with picamera.PiCamera() as camera:
+    # prep picamera
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.rotation   = 180
+        camera.crop       = (0.0, 0.0, 1.0, 1.0)
 
-    camera.resolution = (W, H)
-    camera.framerate = 30
-    camera.start_preview()
+        # display preview
+        camera.start_preview()
 
-    #NB layer argument below, fps as slow as needed for whatever's changing
-    DISPLAY = pi3d.Display.create(w=W, h=H, layer=4, frames_per_second=10)
-    DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
-    shader = pi3d.Shader("uv_flat")
-    CAMERA = pi3d.Camera(is_3d=False)
-    font = pi3d.Font("/usr/share/fonts/truetype/freefont/FreeSerif.ttf",
-                          (128, 255, 128, 255)) # blue green 1.0 alpha
-    keybd = pi3d.Keyboard()
-    tx = -DISPLAY.width / 2 + 150 # incr right, zero in middle
-    ty = DISPLAY.height / 2 - 50 # incr upwards
+        # continuously updates the overlayed layer and display stats
+        overlay_renderer = None
+        while True:
+            text = time.strftime('%H:%M:%S', time.gmtime())
+            img = Image.new("RGB", (1024, 768))
+            draw = ImageDraw.Draw(img)
+            draw.font = ImageFont.truetype(
+                            "/usr/share/fonts/truetype/freefont/FreeSerif.ttf",
+                            50)
+            draw.text((10,10), text, (255, 255, 255))
 
-    while DISPLAY.loop_running():
-      text = pi3d.String(font=font, string=time.strftime('%H:%M:%S', time.gmtime()),
-                      camera=CAMERA, x=tx, y=ty, z=1.0, is_3d=False)
-      text.set_shader(shader)
-      text.draw()
-      time.sleep(0.1)
-      if keybd.read() == 27:
-        DISPLAY.destroy()
-        break
+            if not overlay_renderer:
+                """
+                If overlay layer is not created yet, get a new one. Layer
+                parameter must have 3 or higher number because the original
+                preview layer has a # of 2 and a layer with smaller number will
+                be obscured.
+                """
+                overlay_renderer = camera.add_overlay(img.tostring(),
+                                                      layer=3,
+                                                      size=img.size,
+                                                      alpha=128);
+            else:
+                overlay_renderer.update(img.tostring())
+
+if __name__ == '__main__':
+    import sys
+    try:
+        main()
+    except:
+        print 'Unexpected error : ', sys.exc_info()[0], sys.exc_info()[1]
