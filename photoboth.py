2b3d683dd7d6268c6214f23193713aebff89dd58
Nick Hildebrandt <n.hildebrandt@ts13.de>
2021-05-03 22:26:13 +0200

    BETA 1

--- photoboth.py
+++ photoboth.py
@@ -4,66 +4,82 @@
 import picamera
 import os
 import time
+import json
 from PIL import Image, ImageDraw, ImageFont
 import RPi.GPIO as GPIO
 
+with open("config.json") as file:
+	data = json.load(file)
+
 overlay_renderer = None
+
+	W 			= data["resolution"]["W"]
+	H 			= data["resolution"]["H"]
+	font_size	= data["font"]["size"]
+	font_art	= data["font"]["art"]
+	sleep_time	= data["text"]["sleep_time"]
+	3			= data["text"]["3"]
+	2			= data["text"]["2"]
+	1			= data["text"]["1"]
+	fps			= data["foto"]["fps"]
+	pic			= data["foto"]["pic"]
 
 def main():
 
-    W = 1280
-    H = 720
-    BUTTON_GPIO = 16
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    pressed = False
+	BUTTON_GPIO = 16
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	pressed = False
 
-    def text(text):
-        global overlay_renderer
-        img = Image.new("RGBA", (1280, 720), (255, 0, 0, 0))
-        draw = ImageDraw.Draw(img)
-        draw.font = ImageFont.truetype(
-                        "/usr/share/fonts/truetype/msttcorefonts/Impact.ttf",
-                        80)
-        w, h = draw.textsize(text)
-        draw.text(((W-w)/2,(H-h)/2), text, (255, 255, 255))
+	def text(text):
+		global overlay_renderer
+		img = Image.new("RGBA", (W, H), (255, 0, 0, 0))
+		draw = ImageDraw.Draw(img)
+		draw.font = ImageFont.truetype(font_art, font_size)
+		w, h = draw.textsize(text)
+		draw.text(((W-w)/2,(H-h)/2), text, (255, 255, 255))
 
-        if not overlay_renderer:
-            overlay_renderer = camera.add_overlay(img.tobytes(),
-                                                  layer=3,
-                                                  size=img.size,
-                                                  alpha=128);
-        else:
-            overlay_renderer.update(img.tobytes())
+		if not overlay_renderer:
+			overlay_renderer = camera.add_overlay(img.tobytes(),
+												  layer=3,
+												  size=img.size,
+												  alpha=128);
+		else:
+			overlay_renderer.update(img.tobytes())
 
-    with picamera.PiCamera() as camera:
-        camera.resolution = (W), (H)
-        camera.crop       = (0.0, 0.0, 1.0, 1.0)
-        camera.start_preview()
+	with picamera.PiCamera() as camera:
+		camera.resolution = (W), (H), framerate=fps
+		camera.crop       = (0.0, 0.0, 1.0, 1.0)
+		camera.start_preview()
 
-        overlay_renderer = None
+		overlay_renderer = None
 
-        while True:
-            text(text = "Mache ein Foto mit dem Knopf")
-            if not GPIO.input(BUTTON_GPIO):
-                if not pressed:
-                    text(text = "Auf die Plätze")
-                    time.sleep(1.5)
-                    text(text = "Fertig?")
-                    time.sleep(1.5)
-                    text(text = "Cheese!")
-                    time.sleep(1)
-                    date = ("data/" + (time.strftime("%Y%m%d-%H%M%S")) + ".png")
-                    text(text = " ")
-                    camera.capture((date), use_video_port=False)
-                    time.sleep(3)
+		while True:
+			text(text = "Mache ein Foto mit dem Knopf")
+			if not GPIO.input(BUTTON_GPIO):
+				if not pressed:
+					text(text = 3)
+					time.sleep(sleep_time)
+					text(text = 2)
+					time.sleep(sleep_time)
+					text(text = 1)
+					time.sleep(sleep_time)
+					date = ("data/" + (time.strftime("%Y%m%d-%H%M%S")) + (pic))
+					text(text = " ")
+					camera.shutter_speed = camera.exposure_speed
+					camera.exposure_mode = 'off'
+					g = camera.awb_gains
+					camera.awb_mode = 'off'
+					camera.awb_gains = g
+					camera.capture((date), use_video_port=False)
+					time.sleep(3)
 
 
-        else:
-            pressed = False
-        time.sleep(0.1)
+		else:
+			pressed = False
+		time.sleep(0.1)
 
 
 if __name__ == '__main__':
-    import sys
-    main()
+	import sys
+	main()
